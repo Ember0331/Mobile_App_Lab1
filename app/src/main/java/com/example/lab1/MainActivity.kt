e0b142437a02c58fd8299696245fd0cc8cdd9ed6
Ember0331 <keigot517@gmail.com>
2026-02-19 09:28:32 -0500

    It is all fixed and looks good to me

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -47,6 +48,7 @@ fun ProfileCard() {
         Image(
             painter = painterResource(id = R.drawable.profile_image),
             contentDescription = "Profile picture",
+            contentScale = ContentScale.Crop,
             modifier = Modifier
                 .size(140.dp)
                 .clip(CircleShape)
